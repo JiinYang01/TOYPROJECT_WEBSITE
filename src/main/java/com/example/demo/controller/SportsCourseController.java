adad3461789a9b2fd78f635a8b5b0fe7657aabd2
kyounje <kyounje@naver.com>
2024-11-11 23:36:37 +0900

    타임리프 적용

--- src/main/java/com/example/demo/controller/SportsCourseController.java
+++ src/main/java/com/example/demo/controller/SportsCourseController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,11 +18,25 @@ public class SportsCourseController {
     private final SportsCourseService courseService;
 
     @GetMapping("/trend")
-    public String list(Model model) {
+    public String list(@RequestParam(name = "sortType", defaultValue = "All") String sortType, Model model) {
         List<SportsCourseDTO> courseList = this.courseService.getList();
+
+        switch(sortType) {
+            case "priceAsc":
+                courseList = this.courseService.getCourseSortedByPriceAsc();
+                break;
+            case "priceDesc":
+                courseList = this.courseService.getCourseSortedByPriceDesc();
+                break;
+            case "popularity":
+                courseList = this.courseService.getCourseSortedByCourseReqstNmprCo();
+                break;
+            default:
+                break;
+        }
+
         model.addAttribute("courseList", courseList);
         return "course_trend";
     }
-
 
 }
--- src/main/java/com/example/demo/repository/SportsCourseRepository.java
+++ src/main/java/com/example/demo/repository/SportsCourseRepository.java
@@ -3,5 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.domain.SportsCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SportsCourseRepository extends JpaRepository<SportsCourse, Long> {
+
+    List<SportsCourse> findAllByOrderByCoursePrcAsc();
+
+    List<SportsCourse> findAllByOrderByCoursePrcDesc();
+
+    List<SportsCourse> findAllByOrderByCourseReqstNmprCoDesc();
 }
