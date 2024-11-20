655df9a57bca2fedc7e644e4acaeee0aee6f61a3
kyounje <kyounje@naver.com>
2024-11-20 20:50:53 +0900

    recommend

--- src/main/java/com/example/demo/controller/RecommendationController.java
+++ src/main/java/com/example/demo/controller/RecommendationController.java
@@ -15,19 +15,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/recommend")
 public class RecommendationController {
     private final RecommendationService recommendationService;
+    private final SurveyService surveyService;
 
     @GetMapping("/")
     public String recommendCourses(@AuthenticationPrincipal CustomUserDetails user, Model model) {
         List<SportsCourseDTO> courses = recommendationService.getRecommendedCourses(user);
 
-        System.out.println(courses);
-        model.addAttribute("courseList", courses);
+        SurveyDTO response = surveyService.getResponsesByUserId(user);
+        model.addAttribute("responses", response);
+
+//        List<String> categoryList = courses.stream()
+//                .map(course -> course.getCategory().getCategoryName())
+//                .distinct()
+//                .collect(Collectors.toList());
+//        model.addAttribute("categoryList", categoryList);
+
+        // 카테고리별로 코스를 그룹화
+        Map<String, List<SportsCourseDTO>> groupedCourses = courses.stream()
+                .collect(Collectors.groupingBy(course -> course.getCategory().getCategoryName()));
+
+        // 모델에 그룹화된 데이터를 추가
+        model.addAttribute("groupedCourses", groupedCourses);
+
         return "course_recommend";
     }
 }
